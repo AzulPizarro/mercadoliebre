7821ba829f77b4b5f462474f3fcb04cc4e94b93f
azul pizarro <azulpizarro15@gmail.com>
2021-07-30 16:29:50 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express=require('express')
+const app=express ()
+const port=3030
+const path=require('path')
+
+app.get('/', function(req, res){
+    res.send('Hola')
+})
+
+app.get('/home', function(request, response){
+    response.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
+app.get('/register', function(request, response){
+    response.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get('/login', function(request, response){
+    response.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+app.use(express.static('public'))
+
+app.get('*', function(request, response){
+    response.send('NOT FOUND', 404)
+})
+
+app.listen(port, ()=>{
+    console.log('La app esta funcionando en http://localhost:3030/')
+})
+
